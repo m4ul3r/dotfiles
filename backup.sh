8b9c3f4bbe9f2ccd0b094dbd16c2e19ce4e478fa
tyler <tyler@pop-os.localdomain>
2021-04-29 23:48:32 -0500

    Backup 2021-04-29 23:48:32

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# copy dot files
+cp $HOME/.bashrc .
+cp $HOME/.vimrc .
+
+# check git status
+gs="$(git status | grep -i "modified")"
+#echo "${gs}"
+
+# if changes present
+if [[ $gs == *"modified"* ]]; then
+    echo "push"
+fi
+
+# push to Git
+git add -u;
+git commit -m "Backup `date +'%Y-%m-%d %H:%M:%S'`";
+git push -u origin main
